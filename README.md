71437667fdf23a3739669bbf491fd0c6a3f21f17
this-shahriar <this.parash@gmail.com>
2021-01-08 22:50:14 +0600

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+## **What is it?**
+This repo holds a simple implementation to get started with React using zero config Parcel-bundler
+
+## **Get started**
+To start the development server,
+
+    npm start
+
+Build react app to deploy,
+
+    npm run build
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => (
+  <div className="app">
+    <h2>Welcome to create react parcel app!</h2>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
